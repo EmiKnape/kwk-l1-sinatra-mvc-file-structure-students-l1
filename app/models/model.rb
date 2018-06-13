c9d1b13c200d9b720198928af9bcdb2cba6b0bfe
EmiKnape <enknape@aol.com>
2018-06-13 21:55:59 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,11 @@
-class Model
-  # Replace with Dog clas
-end
+#class Model
+  # Replace with fortune teller
+#end
+
+
+def get_fortune()
+  #METHOD GOES HERE 
+  fortunes_array = ["You'll have a great day", "Someone's going to go to the bathroom in 20 minutes", "You'll have sandwiches for the rest of your life.", "You will be dehydrated from lack of Core water", "Sam will roast Jon today", "You'll have bad luck for the next hour"]
+  return fortunes_array.sample 
+end 
+puts get_fortune 
